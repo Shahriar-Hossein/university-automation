74a2148a2855635825cdb5073f6c5537715fe95e
Shahriar-Hossein <shahriar2507@gmail.com>
2026-04-14 16:20:01 +0600

    replace fixed url with dynamic base url

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,7 +1,14 @@
 <?php
-require_once('dbconnection.php');
+require_once BASE_PATH . '/dbconnection.php';
+
+// Compute a dynamic base URL so links work on any host/port/path
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443) ? 'https' : 'http';
+$base_path = rtrim(dirname(dirname($_SERVER['SCRIPT_NAME'])), '/\\');
+$base_url = $protocol . '://' . $_SERVER['HTTP_HOST'] . ($base_path === '' || $base_path === '/' ? '' : $base_path);
+
 if (!isset($_SESSION['admin_login_id'])) {
-  header('Location: http://localhost/sms/adminlogin.php');
+  header('Location: ' . $base_url . '/adminlogin.php');
+  exit;
 }
 $admin_login_user = $_SESSION['admin_login_id'];
 ?>
@@ -14,12 +21,12 @@ $admin_login_user = $_SESSION['admin_login_id'];
   <ul class="nav-links">
 
     <li>
-      <a href="http://localhost/sms/admin/dashboard.php">
+      <a href="<?php echo $base_url; ?>/admin/dashboard.php">
         <i class='bx bx-grid-alt'></i>
         <span class="link_name">Dashboard</span>
       </a>
       <ul class="sub-menu blank">
-        <li><a class="link_name" href="http://localhost/sms/admin/dashboard.php">Dashboard</a></li>
+        <li><a class="link_name" href="<?php echo $base_url; ?>/admin/dashboard.php">Dashboard</a></li>
       </ul>
     </li>
 
@@ -33,9 +40,9 @@ $admin_login_user = $_SESSION['admin_login_id'];
       </div>
       <ul class="sub-menu">
         <li><a class="link_name">Course</a></li>
-        <li><a href="http://localhost/sms/admin/pages/addCourse.php">Add Course</a></li>
-        <li><a href="http://localhost/sms/admin/pages/manageCourse.php">Manage Course</a></li>
-        <li><a href="http://localhost/sms/admin/pages/manageSection.php">Manage Section</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/addCourse.php">Add Course</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/manageCourse.php">Manage Course</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/manageSection.php">Manage Section</a></li>
       </ul>
     </li>
 
@@ -51,8 +58,8 @@ $admin_login_user = $_SESSION['admin_login_id'];
       </div>
         <ul class="sub-menu">
           <li><a class="link_name">Rooms</a></li>
-          <li><a href="http://localhost/sms/admin/pages/addRoom.php">Add Room</a></li>
-          <li><a href="http://localhost/sms/admin/pages/manageRooms.php">Manage Room</a></li>
+          <li><a href="<?php echo $base_url; ?>/admin/pages/addRoom.php">Add Room</a></li>
+          <li><a href="<?php echo $base_url; ?>/admin/pages/manageRooms.php">Manage Room</a></li>
         </ul>
       </li>
 
@@ -66,8 +73,8 @@ $admin_login_user = $_SESSION['admin_login_id'];
       </div>
       <ul class="sub-menu">
         <li><a class="link_name">Schedule</a></li>
-        <li><a href="http://localhost/sms/admin/pages/teacherSchedule.php">Teacher Schedule</a></li>
-        <li><a href="http://localhost/sms/admin/pages/courseSchedule.php">Course Schedule</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/teacherSchedule.php">Teacher Schedule</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/courseSchedule.php">Course Schedule</a></li>
       </ul>
     </li>
 
@@ -81,8 +88,8 @@ $admin_login_user = $_SESSION['admin_login_id'];
       </div>
       <ul class="sub-menu">
         <li><a class="link_name" href="#">Teachers</a></li>
-        <li><a href="http://localhost/sms/admin/pages/addTeachers.php">Add Teachers</a></li>
-        <li><a href="http://localhost/sms/admin/pages/manageTeachers.php">Manage Teachers</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/addTeachers.php">Add Teachers</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/manageTeachers.php">Manage Teachers</a></li>
 
       </ul>
     </li>
@@ -98,10 +105,10 @@ $admin_login_user = $_SESSION['admin_login_id'];
       </div>
       <ul class="sub-menu">
         <li><a class="link_name" href="#">Students</a></li>
-        <li><a href="http://localhost/sms/admin/pages/addStudents.php">Add Students</a></li>
-        <li><a href="http://localhost/sms/admin/pages/manageStudents.php">Manage Students</a></li>
-        <li><a href="http://localhost/sms/admin/pages/attendance.php">Attendance</a></li>
-        <li><a href="http://localhost/sms/admin/pages/paymentHistory.php">Payment History</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/addStudents.php">Add Students</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/manageStudents.php">Manage Students</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/attendance.php">Attendance</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/paymentHistory.php">Payment History</a></li>
       </ul>
     </li>
 
@@ -116,18 +123,18 @@ $admin_login_user = $_SESSION['admin_login_id'];
       </div>
       <ul class="sub-menu">
         <li><a class="link_name" href="#">Publice Notice</a></li>
-        <li><a href="http://localhost/sms/admin/pages/addPublicNotice.php">Add Public Notice</a></li>
-        <li><a href="http://localhost/sms/admin/pages/managePublicNotice.php">Manage Public Notice</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/addPublicNotice.php">Add Public Notice</a></li>
+        <li><a href="<?php echo $base_url; ?>/admin/pages/managePublicNotice.php">Manage Public Notice</a></li>
       </ul>
     </li>
 
     <li>
-      <a href="http://localhost/sms/admin/pages/search.php">
+      <a href="<?php echo $base_url; ?>/admin/pages/search.php">
         <i class='bx bx-search-alt'></i>
         <span class="link_name">Search</span>
       </a>
       <ul class="sub-menu blank">
-        <li><a class="link_name" href="http://localhost/sms/admin/pages/search.php">Search</a></li>
+        <li><a class="link_name" href="<?php echo $base_url; ?>/admin/pages/search.php">Search</a></li>
       </ul>
     </li>
 
@@ -145,7 +152,7 @@ $admin_login_user = $_SESSION['admin_login_id'];
           </div>
           <div class="job">Admin</div>
         </div>
-        <a href="http://localhost/sms/logout.php"><i class='bx bx-log-out'></i></a>
+        <a href="<?php echo $base_url; ?>/logout.php"><i class='bx bx-log-out'></i></a>
       </div>
     </li>
 
