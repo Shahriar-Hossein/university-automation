2d359195f2bd4687bfaf7d12c2b8d62a05443a45
Shahriar-Hossein <shahriar2507@gmail.com>
2026-04-14 18:18:23 +0600

    fix icon alignment

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -59,7 +59,7 @@ $admin_login_user = $_SESSION['admin_login_id'];
     <li>
       <div class="iocn-link">
         <a>
-          <i> 
+          <i style="display:flex; align-items:center; justify-content:center"> 
             <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M19 19V4h-4V3H5v16H3v2h12V6h2v15h4v-2zm-6 0H7V5h6zm-3-8h2v2h-2z"/></svg>
           </i>
           <span class="link_name">Rooms</span>
