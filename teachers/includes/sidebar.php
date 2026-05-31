2c214f328f6cbcb8e5b485d05c3748fbf9675d81
Shahriar-Hossein <shahriar2507@gmail.com>
2026-06-01 04:30:55 +0600

    fix: instructor sidebar link issues

--- teachers/includes/sidebar.php
+++ teachers/includes/sidebar.php
@@ -2,7 +2,43 @@
 
 $teacher_login_user = $_SESSION['teacher_login_id'];
 
-$teacher_result = mysqli_query($conn, "SELECT * FROM `teacher_db` WHERE `t_ID` = '$teacher_login_user'");
+// compute current request path and area prefix for building links
+$currentPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$scriptSegments = explode('/', trim($_SERVER['SCRIPT_NAME'], '/'));
+$firstSeg = isset($scriptSegments[0]) ? $scriptSegments[0] : '';
+$knownAreas = ['students', 'teachers', 'admin'];
+if (in_array($firstSeg, $knownAreas)) {
+  $appRoot = '';
+} else {
+  $appRoot = $firstSeg ? '/' . $firstSeg : '';
+}
+
+if (strpos($currentPath, '/teachers/') !== false || $currentPath === '/teachers' || $currentPath === '/teachers/') {
+  $areaPrefix = $appRoot . '/teachers';
+} else {
+  $areaPrefix = $appRoot;
+}
+
+function buildTeacherLink($path)
+{
+  global $areaPrefix;
+  return rtrim($areaPrefix, '/') . $path;
+}
+
+function isTeacherActive($paths)
+{
+  $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+  $paths = (array)$paths;
+  foreach ($paths as $p) {
+    if ($uri === $p || strpos($uri, $p) === 0) return true;
+    global $areaPrefix;
+    $full = rtrim($areaPrefix, '/') . $p;
+    if ($uri === $full || strpos($uri, $full) === 0) return true;
+  }
+  return false;
+}
+
+$teacher_result = mysqli_query($conn, "SELECT * FROM `teacher_db` WHERE `t_ID` = '" . mysqli_real_escape_string($conn, $teacher_login_user) . "'");
 $teacher_login_user_data = mysqli_fetch_assoc($teacher_result);
 
 ?>
@@ -15,80 +51,80 @@ $teacher_login_user_data = mysqli_fetch_assoc($teacher_result);
   <ul class="nav-links">
 
     <li>
-      <a href="<?= BASE_URL ?>teachers/dashboard.php">
+      <a href="<?= buildTeacherLink('/dashboard.php') ?>" class="<?= isTeacherActive('/dashboard.php') ? 'active' : '' ?>">
         <i class='bx bx-grid-alt'></i>
         <span class="link_name">Dashboard</span>
       </a>
       <ul class="sub-menu blank">
-        <li><a class="link_name" href="<?= BASE_URL ?>teachers/dashboard.php">Dashboard</a></li>
+        <li><a class="link_name" href="<?= buildTeacherLink('/dashboard.php') ?>">Dashboard</a></li>
       </ul>
     </li>
 
     <li>
       <div class="iocn-link">
-        <a href="<?= BASE_URL ?>teachers/pages/profile.php">
+        <a href="<?= buildTeacherLink('/pages/profile.php') ?>" class="<?= isTeacherActive(['/pages/profile.php', '/pages/edit_profile.php']) ? 'active' : '' ?>">
           <i class='bx bxs-user-rectangle'></i>
           <span class="link_name">Profile</span>
         </a>
       </div>
       <ul class="sub-menu">
-        <li><a class="link_name" href="<?= BASE_URL ?>teachers/pages/profile.php">Profile</a></li>
+        <li><a class="link_name" href="<?= buildTeacherLink('/pages/profile.php') ?>">Profile</a></li>
       </ul>
     </li>
 
     <li>
       <div class="iocn-link">
-        <a href="<?= BASE_URL ?>teachers/pages/viewCourse.php">
+        <a href="<?= buildTeacherLink('/pages/viewCourse.php') ?>" class="<?= isTeacherActive(['/pages/viewCourse.php', '/pages/courseSchedule.php']) ? 'active' : '' ?>">
           <i class='bx bx-message-alt-minus'></i>
           <span class="link_name">Course</span>
         </a>
         <i class='bx bxs-chevron-down arrow'></i>
       </div>
-      <ul class="sub-menu">
-        <li><a class="link_name" href="<?= BASE_URL ?>teachers/pages/viewCourse.php">Course</a></li>
-        <li><a href="<?= BASE_URL ?>teachers/pages/viewCourse.php">View Course</a></li>
-        <li><a href="<?= BASE_URL ?>teachers/pages/courseSchedule.php">Course Schedule</a></li>
+      <ul class="sub-menu <?= isTeacherActive(['/pages/viewCourse.php', '/pages/courseSchedule.php']) ? 'show' : '' ?>">
+        <li><a class="link_name" href="<?= buildTeacherLink('/pages/viewCourse.php') ?>">Course</a></li>
+        <li><a href="<?= buildTeacherLink('/pages/viewCourse.php') ?>">View Course</a></li>
+        <li><a href="<?= buildTeacherLink('/pages/courseSchedule.php') ?>">Course Schedule</a></li>
       </ul>
     </li>
 
     <li>
       <div class="iocn-link">
-        <a href="<?= BASE_URL ?>teachers/pages/attendance.php">
+        <a href="<?= buildTeacherLink('/pages/attendance.php') ?>" class="<?= isTeacherActive('/pages/attendance.php') ? 'active' : '' ?>">
           <i class='bx bx-check-square'></i>
           <span class="link_name">Attendance</span>
         </a>
       </div>
       <ul class="sub-menu">
-        <li><a class="link_name" href="<?= BASE_URL ?>teachers/pages/attendance.php">Attendance</a></li>
+        <li><a class="link_name" href="<?= buildTeacherLink('/pages/attendance.php') ?>">Attendance</a></li>
       </ul>
     </li>
 
     <li>
       <div class="iocn-link">
-        <a href="">
+        <a href="<?= buildTeacherLink('/pages/addNotice.php') ?>" class="<?= isTeacherActive(['/pages/addNotice.php', '/pages/manageNotice.php']) ? 'active' : '' ?>">
           <i class='bx bx-message-alt-minus'></i>
           <span class="link_name">Notice</span>
         </a>
         <i class='bx bxs-chevron-down arrow'></i>
       </div>
-      <ul class="sub-menu">
-        <li><a class="link_name" href="#">Notice</a></li>
-        <li><a href="<?= BASE_URL ?>teachers/pages/addNotice.php">Add Notice</a></li>
-        <li><a href="<?= BASE_URL ?>teachers/pages/manageNotice.php">Manage Notice</a></li>
+      <ul class="sub-menu <?= isTeacherActive(['/pages/addNotice.php', '/pages/manageNotice.php']) ? 'show' : '' ?>">
+        <li><a class="link_name" href="<?= buildTeacherLink('/pages/addNotice.php') ?>">Notice</a></li>
+        <li><a href="<?= buildTeacherLink('/pages/addNotice.php') ?>">Add Notice</a></li>
+        <li><a href="<?= buildTeacherLink('/pages/manageNotice.php') ?>">Manage Notice</a></li>
       </ul>
     </li>
 
     <li>
       <div class="iocn-link">
-        <a href="">
-        <i class='bx bx-edit-alt'></i>
+        <a href="<?= buildTeacherLink('/pages/grading.php') ?>" class="<?= isTeacherActive('/pages/grading.php') ? 'active' : '' ?>">
+          <i class='bx bx-edit-alt'></i>
           <span class="link_name">Grading</span>
         </a>
         <i class='bx bxs-chevron-down arrow'></i>
       </div>
-      <ul class="sub-menu">
-        <li><a class="link_name" href="#">Grading</a></li>
-        <li><a href="<?= BASE_URL ?>teachers/pages/grading.php">Upload Grading</a></li>
+      <ul class="sub-menu <?= isTeacherActive('/pages/grading.php') ? 'show' : '' ?>">
+        <li><a class="link_name" href="<?= buildTeacherLink('/pages/grading.php') ?>">Grading</a></li>
+        <li><a href="<?= buildTeacherLink('/pages/grading.php') ?>">Upload Grading</a></li>
       </ul>
     </li>
 
@@ -101,12 +137,9 @@ $teacher_login_user_data = mysqli_fetch_assoc($teacher_result);
           <div class="profile_name"><?= $teacher_login_user_data['t_Name'] ?></div>
           <div class="job">Teacher</div>
         </div>
-        <a href="<?= BASE_URL ?>logout.php"><i class='bx bx-log-out' style="padding-right: 20px;"></i></a>        
+        <a href="<?= $appRoot . '/logout.php' ?>"><i class='bx bx-log-out' style="padding-right: 20px;"></i></a>
       </div>
     </li>
-
-
-    
 
   </ul>
 </div>
